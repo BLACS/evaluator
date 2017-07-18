871ed729f3f4adf4f3f1d8e78936dfa4b4cb434a
Colin González <colin.gonzalez@openmailbox.org>
2017-07-18 18:11:26 +0200

    Simple evaluator implentation in place using liboblacs

---
+++ src/blacs.ml
@@ -0,0 +1,79 @@
+open Yojson.Safe
+
+let args    = Sys.argv
+
+let sheet   = Array.get args 2
+
+let tag     = Array.get args 3
+
+let default = Array.get args 4
+
+module BHost =
+struct
+  let host = Array.get Sys.argv 1
+end
+
+module BPervasives = BlacsPervasives.Make(BHost)
+
+let get_size () =
+  let open Dimension in
+  match BPervasives.io_size sheet with
+    Result.Ok { length=l ; width=w }  -> l,w
+  | Result.Error e -> failwith e
+
+let get_time () =
+  let open Dimension in
+  match BPervasives.clock_time sheet with
+    Result.Ok  t -> t
+  | Result.Error e -> failwith e
+
+let get_hash hash =
+  match BPervasives.io_hash sheet hash with
+    Result.Ok lvl -> lvl
+  | Result.Error e -> failwith e
+
+let read_and_hash rrq =
+  let open ReadPromise in
+   match BPervasives.io_read sheet rrq with
+    Result.Ok rp ->
+    (if rp.date > (Unix.gettimeofday ())
+     then
+       Unix.sleepf (rp.date -. (Unix.gettimeofday ()) +. 0.005)
+     else ());
+    get_hash rp.hash
+  | Result.Error e -> failwith e
+
+let read_sheet time length width =
+  let open ReadRequest in
+  let rrq = read_request ~tag:tag ~time:time
+      ~origin:(Coordinates.coords 0 0) ~length:length
+      ~width:width ~filter_formulas:false ~default:(Some default) in
+  read_and_hash rrq
+
+let read_formulas time width length =
+  let open ReadRequest in
+  let rrq = read_request ~tag:tag ~time:time
+      ~origin:(Coordinates.coords 0 0) ~length:length
+      ~width:width ~filter_formulas:true ~default:(Some default) in
+  read_and_hash rrq
+
+let read time origin length width =
+  let open ReadRequest in
+  let rrq = read_request ~tag:tag ~time:time
+      ~origin:origin ~length:length
+      ~width:width ~filter_formulas:false ~default:(Some default) in
+  read_and_hash rrq
+
+let write_sheet time length width values =
+  let wrq = WriteRequest.write_request ~tag:tag
+      ~time:time ~origin:(Coordinates.coords 0 0)
+      ~length:length ~width:width ~values:values  in
+  BPervasives.io_write sheet wrq
+
+let write time origin length width values =
+  let wrq = WriteRequest.write_request ~tag:tag
+      ~time:time ~origin:origin
+      ~length:length ~width:width ~values:values in
+  BPervasives.io_write sheet wrq
+
+  
--- src/evaluator.ml
+++ src/evaluator.ml
@@ -1,304 +1,104 @@
-module HTC = Hashtbl.Make(Coordinates)
+open Blacs
 
-let args = Sys.argv
-
-let url        = Array.get args 1
-
-let sheet      = Array.get args 2
-
-let tag        = Array.get args 3
-
-let default    = Array.get args 4
-
-let httpHeader = ["accept: application/json"; "content-type: application/json"]
-
-type color = Black | Grey | White
-
-
-type readRQ           = {tag          : string        ;
-                         time         : Nativeint.t   ;
-                         origin       : Coordinates.t ;
-                         width        : int           ;
-                         length       : int           ;
-                         default      : string option } [@@deriving yojson]
-
-type writeRQ          = {tag          : string        ;
-                         time         : Nativeint.t   ;
-                         origin       : Coordinates.t ;
-                         length       : int           ;
-                         width        : int           ;
-                         values       : Value.t list  } [@@deriving yojson]
-
-type readPromise      =  {date        : float         ;
-                          hash        : string        } [@@deriving yojson]
-
-type locatedValue     = {coords       : Coordinates.t ;
-                         value        : Value.t       } [@@deriving yojson]
-
-type locatedValueList =  locatedValue list  [@@deriving yojson]
+type color = Black | White
 
 let bind x f = match x with
     Some v -> f v
   | None   -> None
 
-let ( >>= )  = bind
+let marks = Hashtbl.create 13
 
-let return   = fun x -> Some x
-
-let readrq tag time origin width length default =
-  {tag           = tag     ;
-   time          = time    ;
-   origin        = origin  ;
-   width         = width   ;
-   length        = length  ;
-   default       = default }
-
-let writerq tag time origin width length values =
-  {tag           = tag     ;
-   time          = time    ;
-   origin        = origin  ;
-   width         = width   ;
-   length        = length  ;
-   values        =  values }
-
-let filterTable table predicate =
-  let filter c v acc =
-    if  predicate c v
-    then c::acc
-    else acc
-  in
-  return (HTC.fold filter table [])
-
-let isCount c v = Value.(
-  match v with
-    _, { ty = TyCount; data = Some _ } -> true
-  | _                                  -> false)
-
-let filterCountFormulas values =
-  filterTable values isCount
-
-let inRange inf sup c v =
-  Coordinates.(lteq inf c && lteq c sup)
-
-let dependencies f formulas =
-  let open Value                                 in
-  let open Coordinates                           in
-  match f with
-    {ty=TyCount; data=Some([r1;c1;width;length;_])} ->
-    let inf,sup = coords r1 c1, coords (r1+width-1) (c1+length-1) in
-    let range c = lteq inf c && lteq c sup in
-    return (List.filter range formulas)
-  | _ ->  None
-
-let findSchedule values formulas coord =
-  let rec aux output vertex =
-    let (color,formula) = HTC.find values vertex     in
-    match color,dependencies formula formulas with
-      Black,_          -> output
-    | Grey,_           ->
-      HTC.replace values vertex (Grey,formula);
-      None
-    | White,None       ->
-      output                                         >>= fun output ->
-      HTC.replace values vertex (Black,formula);
-      return (vertex::output)
-    | White,Some(deps) ->
-      HTC.replace values vertex (Grey,formula)       ;
-      List.fold_left aux output deps                 >>= fun output -> 
-      HTC.replace values vertex (Black,formula)      ;
-      return (vertex::output)    
-  in
-  try
-    aux (return []) coord
-  with
-    Not_found -> None
-
-let eval c values =
-  let open Value                                   in
-  let open Coordinates                             in
-  let f = HTC.find values c                        in
-  match f with
-    Grey,_ ->
-    None
-  | _,{ ty=TyCount ; data=Some ([r1;c1;width;length;i]) } ->
-    let inf,sup = (coords r1 c1),(coords (r1+width-1) (c1+length-1))    in
-    let inRange = inRange inf sup                  in
-    let predicate c v = match v with
-        _,{ ty=TyInt ; data = Some [i'] }          ->
-        inRange c v && i=i' 
-      | _ -> false
-    in
-    filterTable values predicate                   >>= fun l ->
-    return (Value.int (List.length l))
-  | _,_    -> None
-
-let valuesTable vl vt =
-  List.iter (fun {coords=c; value=v} -> HTC.add vt c (White,v)) vl;
-  vt
-
-let evalSchedule vt s  =
-  let s = List.rev s in 
-  List.iter
-    (fun x ->
-       match eval x vt with
-         Some v -> HTC.replace vt x (White,v)
-       | None   -> ()) s
-
-
-let httpConnection url writeFunction httpHeader =
-  let open Curl in
-  let connection = init () in
-  set_url connection url;
-  set_writefunction connection writeFunction;
-  set_httpheader connection httpHeader;
-  connection
-
-let postConnection url writeFunction httpHeader httpBody =
-  let open Curl in
-  let connection = httpConnection url writeFunction httpHeader      in
-  set_post connection true;
-  set_postfields connection httpBody;
-  connection
-
-
-let getConnection url writeFunction httpHeader getParams =
-  let open Curl in
-  let urlParams =
-    (function
-      None -> url
-    | Some params -> Printf.sprintf "%s/%s" url params) getParams    in
-  let connection = httpConnection urlParams writeFunction httpHeader in
-  connection
-
-let performConnection connection =
-  let open Curl in
-  perform connection;
-  get_responsecode connection
-    
-let getSize () = 4,11
-
-
-let getTime () =
-  let timeSheet      = return  (Printf.sprintf "time/%s" sheet)      in
-  let buffer         = Buffer.create 128                             in
-  let write          = fun data ->
-    Buffer.add_string buffer data;
-    String.length data                                               in
-  let connection     = getConnection url write httpHeader timeSheet  in
-  let _              = performConnection connection                  in
-  Curl.cleanup connection;
-  let bufferContents = Buffer.contents buffer                        in
-  Nativeint.of_string bufferContents
-
-let getHash hash buffSize =
-  let sheetHash  = return (Printf.sprintf "%s/%s" sheet hash)        in
-  let buffer         = Buffer.create buffSize                        in
-  let write          = fun data ->
-    Buffer.add_string buffer data;
-    String.length data                                               in
-  let connection     = getConnection url write httpHeader sheetHash  in
-  let _              = performConnection connection                  in
-  Curl.cleanup connection;
-  let bufferContents = Buffer.contents buffer                        in
-  let yojson         = Yojson.Safe.from_string bufferContents        in
-  match locatedValueList_of_yojson yojson with
-    Result.Ok(vl) -> vl
-  | _ -> assert false
-
-let readSheet time width length =
-  let buffer = Buffer.create 512                                    in
-  let write data =
-    Buffer.add_string buffer data;
-    String.length data                                              in
-  let rrq            = readrq tag time
-      (Coordinates.coords 0 0) width length (return default)        in
-  let httpBody       =
-    readRQ_to_yojson rrq |> Yojson.Safe.to_string                   in
-  let url = Printf.sprintf "%s/read/%s" url sheet                   in
-  let connection     = postConnection url write httpHeader httpBody in
-  let _ = performConnection connection                           in
-  Curl.cleanup connection;
-  let bufferContents = Buffer.contents buffer                       in
-  let yojson         = Yojson.Safe.from_string bufferContents       in
-  let rp             = match readPromise_of_yojson yojson with
-      Result.Ok(rp) -> rp
-    | _ -> assert false in
-  (if rp.date > (Unix.gettimeofday ())
-   then
-     Unix.sleepf (rp.date -. (Unix.gettimeofday ()) +. 0.5)
-   else ());
-  getHash rp.hash 2048
-
-let writeSheet time width length values =
-  let buffer = Buffer.create 2                                       in
-  let write data =
-    Buffer.add_string buffer data;
-    String.length data                                               in
-  let wrq            = writerq tag time
-      (Coordinates.coords 0 0) width length values                   in
-  let httpBody       =
-    writeRQ_to_yojson wrq |> Yojson.Safe.to_string                   in
-  let url = Printf.sprintf "%s/write/%s" url sheet                   in
-  let connection     = postConnection url write httpHeader httpBody  in
-  let _ = performConnection connection                               in
-  Curl.cleanup connection
-
-let values  () =
-  let width,length = getSize () in
-  let time         = getTime () in
-  return (readSheet time width length)
-
-let writeValues values =
-  let width,length = getSize () in
-  let time         = getTime () in
-  writeSheet time width length values
-
-
-let compareLocVal (c1,_) (c2,_) =
-  Coordinates.compare c1 c2
+let rec eval_formula formulas located_cell =
+  let open LocatedCell in
+  let open Cell        in
+  let def    = located_cell.cell.definition in
+  let coords = located_cell.coords          in
+  if is_evaluated coords then
+    ()
+  else if is_being_evaluated coords then
+    error coords def 
+  else (
+    mark_cell_as_being_evaluated coords;
+    List.iter (eval_formula formulas) (dependencies formulas def);
+    assign_cell coords (eval_formula_definition def)
+  )
   
+and dependencies formulas f =
+  let open Coordinates in
+  match f  with
+    Definition.({ty=TyCount; data=Some [c;r;width;length;_] }) ->
+    let inf,sup = coords c r, coords (c+length-1) (r+width-1)  in
+    let range LocatedCell.({coords=c}) = inf <= c &&  c <= sup in
+    List.filter range formulas
+  | _ ->  []
 
-            
+
+and count i l =
+  let open Cell        in
+  let open Value       in
+  let open LocatedCell in
+  let x = ref 0 in
+  let rec aux l = match l with
+      {cell = {value = Some {ty = TyInt; data= Some[i']}}}::tl ->
+      if i = i' then incr x
+      else (); aux tl
+    | _::tl -> aux tl
+    | [] -> ()
+  in aux l;
+  !x
+           
+and eval_formula_definition f =
+  match f with
+  Definition.({ ty = TyCount; data = Some [c; r; length; width; v] }) ->
+  let cells = read Nativeint.zero (Coordinates.coords c r) length width in
+  let i = count v cells in
+  Cell.cell ~v:i f
+  | _ -> Cell.cell f
+
+and error coord f =
+  let c =
+    Cell.({definition=f; value=Some Value.null_int}) in
+  assign_cell coord c
+
+and assign_cell coord cell =
+  write Nativeint.zero coord 1 1 [cell];
+  mark_cell_as_evaluated coord
+
+and mark_cell_as_being_evaluated coord =
+  Hashtbl.add marks coord Black
+
+and mark_cell_as_evaluated coord =
+  Hashtbl.replace marks coord Black
+
+and is_being_evaluated coord =
+  try
+    Black = Hashtbl.find marks coord
+  with
+  Not_found -> false
+    
+and is_evaluated coord =
+  try
+    White = Hashtbl.find marks coord
+  with
+  Not_found -> false
+
+let init () =
+  let cell  = Cell.cell        in
+  let int   = Definition.int   in
+  let count = Definition.count in
+  let l =
+    [cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 0 1 4 1);
+     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 1 1 4 2);
+     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 2 1 4 3);
+     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 3 1 4 4);
+    ] in
+  write_sheet Nativeint.zero 4 4 l
+
 let main () =
-  let open Value in
-  let l = [int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; count 1 0 3 11 1;
-           int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; count 2 0 1 11 2;
-           int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; count 3 0 1 11 3;
-           int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; count 3 0 1 10 4;
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11          ; *)
-           (* int 1; int 2;int 3; int 4; int 5;int 6; int 7;int 8; int 9;int 10; int 11 *)          ] in
-  writeValues l;
-  let vals = values () in
-  let valSchedOpt =
-    vals                                                      >>= fun vals ->
-    return (valuesTable vals (HTC.create (List.length vals))) >>= fun vt   ->
-    filterCountFormulas vt                                    >>= fun f    ->
-    return ( vt, List.map (findSchedule vt f) f)                                   in
-  (match valSchedOpt with
-     Some (vt,s) ->
-     List.iter (function None   -> ()| Some l -> (evalSchedule vt) l) s;
-     HTC.iter (fun c (col,v) -> match col with Grey -> HTC.replace vt c (Grey,nullInt) | _ -> ()) vt;
-     let values = HTC.fold (fun c (col,v) acc -> (c,v)::acc) vt [] in
-     let values = List.sort compareLocVal values  in
-     (* List.iter (fun (c,v) -> print_endline ( *)
-     (*     Value.string_of_value v ^ " @ " ^ Coordinates.to_string c);flush_all ()) values; *)
-     let values = snd (List.split values) in
-     writeValues values
-   | None -> ())
+  init ();
+  let time         = get_time ()                     in
+  let length,width = get_size ()                     in
+  let formulas = read_formulas time length width     in
+  List.iter (eval_formula formulas) formulas 
 
 let () = main ()
