4dece10f99c0d91a6ab6d7235b7c6574dda55cf0
Colin González <colin.gonzalez@openmailbox.org>
2017-07-27 17:22:19 +0200

    Fixes a bug where a not well defined formula is being evaluated, possible infinite loops

--- src/evaluator.ml
+++ src/evaluator.ml
@@ -4,6 +4,8 @@ type color = Black | White
 
 let marks = Hashtbl.create 13
 
+exception Not_well_defined
+
 let rec eval_formula formulas lcell =
   let open LocatedCell in
   let open Cell        in
@@ -12,22 +14,25 @@ let rec eval_formula formulas lcell =
   if is_evaluated coords then
     ()
   else if is_being_evaluated coords then
-    error coords def 
+    (error coords def;
+     raise Not_well_defined)
   else (
     mark_cell_as_being_evaluated coords;
-    List.iter (eval_formula formulas) (dependencies formulas def);
-    assign_cell coords (eval_formula_definition def)
+    try
+      List.iter (eval_formula formulas) (dependencies formulas def);
+      assign_cell coords (eval_formula_definition def)
+    with
+    Not_well_defined -> ()
   )
   
 and dependencies formulas f =
   let open Coordinates in
   match f  with
     Definition.({ty=TyCount; data=Some [c;r;width;length;_] }) ->
-    let inf,sup = coords c r, coords (c+length-1) (r+width-1)  in
+    let inf,sup = coords c r, coords (c+length) (r+width)  in
     let range LocatedCell.({coords=c}) = inf <= c &&  c <= sup in
     List.filter range formulas
   | _ ->  []
-
 
 and count i l =
   let open Cell        in
@@ -63,11 +68,11 @@ and mark_cell_as_being_evaluated coord =
   Hashtbl.add marks coord Black
 
 and mark_cell_as_evaluated coord =
-  Hashtbl.replace marks coord Black
+  Hashtbl.replace marks coord White
 
 and is_being_evaluated coord =
   try
-    Black = Hashtbl.find marks coord
+    Black = (Hashtbl.find marks coord)
   with
   Not_found -> false
     
@@ -82,13 +87,13 @@ let init () =
   let int   = Definition.int   in
   let count = Definition.count in
   let l =
-    [cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 0 1 4 1);
-     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 1 1 4 2);
-     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 2 1 4 3);
-     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 3 1 4 4);
+    [cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 0 4 4 1);
+     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 1 3 1 2);
+     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 2 3 1 3);
+     cell ~v:1 (int 1); cell ~v:2 (int 2);cell ~v:3 (int 3); cell (count 0 3 3 1 4);
     ] in
   write_sheet Nativeint.zero 4 4 l
-
+    
 let main () =
   init ();
   let time         = get_time ()                     in
